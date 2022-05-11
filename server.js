6c78def33553266b41cfc53ddbdf7eb3dd0fa4bb
Jean Carlos Jimenez <jea_rod5@jeans-mbp.mynetworksettings.com>
2022-05-11 18:01:39 -0400

    modifications

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('systems-list.json');
 const middlewares = jsonServer.defaults();
-const prot = process.env.PORT || 3000;
+const prot = process.env.PORT || 4000;
 
 server.use(middlewares);
 server.use(router);
